bf9c89ceab7aeb3c0ebbf2b0e62ddbb98fdf4030
Vittorio Lo Mele <vittorio@mrbackslash.it>
2020-10-13 15:24:33 +0200

    Adding License + new readme

--- account/2fasettings/index.php
+++ account/2fasettings/index.php
@@ -1,4 +1,22 @@
 <?php
+/*  
+    CloudBooks. Open source hotel and restaurant management software.
+    Copyright (C) 2020 Vittorio Lo Mele
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    Contact me at: vittorio[at]mrbackslash.it
+*/
 if (!file_exists("../../config.php")) {
     die("Please run installer in /installer directory");
 }
@@ -14,7 +32,7 @@ if (!isset($_SESSION["loggedin"]) | $_SESSION["loggedin"] != true) {
 if ($_SESSION["2fa"] == "tocheck") {
     header("location: " . $INSTALL_LINK . "2fa/");
 }
-$err = "null";
+$err = "none";
 //get 2fa status from DB
 $gauth = new PHPGangsta_GoogleAuthenticator;
 $accid = $SQLINK->escape_string($_SESSION["id"]);
@@ -213,6 +231,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["mode"]) && isset($_POS
                 </div>
                 <?php
                 if ($tfa_final_state != "2") {
+                    var_dump($err);
                     displayEnable2faForm($imgsrc, $err);
                 } else {
                     displayDisable2faForm($err);
--- logout.php
+++ logout.php
@@ -1,4 +1,22 @@
 <?php
+/*  
+    CloudBooks. Open source hotel and restaurant management software.
+    Copyright (C) 2020 Vittorio Lo Mele
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    Contact me at: vittorio[at]mrbackslash.it
+*/
 session_start();
 $_SESSION = array();
 session_destroy();
